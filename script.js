84bf5e458b2562056f2bdd9fd3c4a1935c088eee
Christina-Edwards <cedwards239563@gmail.com>
2025-10-12 15:45:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Proof of life
+console.log("hello world");
 
+// Global state object
+const state = {};
+console.table(state); // Proof of life 1
+
+// Factory function (Bonus)
+function createPetOwner(data) {
+  return {
+    ...data,
+    status: function () {
+      console.log(`Owner: ${this.name}, Pet: ${this.petName}, Type: ${this.petType}`);
+    }
+  
