211d3ec3b2295d3e9d8ac3bd080245ac590cca19
Christina-Edwards <cedwards239563@gmail.com>
2025-10-12 15:46:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ function createPetOwner(data) {
     status: function () {
       console.log(`Owner: ${this.name}, Pet: ${this.petName}, Type: ${this.petType}`);
     }
-  
+  };
+}
+
+// Save form data
+function saveForm() {
+  console.log("button clicked!"); // Proof of life
+
+  const name = document.getElementById("ownerName").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const city = document.getElementById("city").value;
+  const zip = document.getElementById("zip").value;
+  const petName = document.getElementById("petName").value;
+  const petType = document.getElementById("petType").value;
+
